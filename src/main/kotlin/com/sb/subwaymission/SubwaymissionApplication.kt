b4431ba7ca621f91ff0cd513f66ca4bcc68cf6c5
kevstevie <jianna442@gmail.com>
2024-07-31 12:45:01 +0900

    initial commit

---
+++ src/main/kotlin/com/sb/subwaymission/SubwaymissionApplication.kt
@@ -0,0 +1,11 @@
+package com.sb.subwaymission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubwaymissionApplication
+
+fun main(args: Array<String>) {
+    runApplication<SubwaymissionApplication>(*args)
+}
---
+++ src/test/kotlin/com/sb/subwaymission/SubwaymissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sb.subwaymission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubwaymissionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
